6b2755fe5ae79838f310c36ec0b1ffc2867c8f04
NASHELAPTOP\Usuario <nachogamallolafon123@gmail.com>
2024-12-10 16:02:00 +0100

    Inicio del programa.

--- Structure/src/main/java/org/example/Main.java
+++ Structure/src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-
-
+        int [] values;
+        String[][] MinesSearch;
+        System.out.println("--------- BIENVENIDO AL BUSCA MINAS ---------");
+        values = Functions.UserEntry();
+        MinesSearch = Functions.GenerateBoard(values[0],values[1]);
     }
 }
